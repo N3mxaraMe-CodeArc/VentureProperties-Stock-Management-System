5db9b801e4540cb1f0712c9c37d872c4881ffff5
N3mxara <nimsara.20223294@iit.ac.lk>
2023-06-03 13:16:03 +0530

    git push

--- src/Component/SecStockForm.jsx
+++ src/Component/SecStockForm.jsx
@@ -1,7 +1,10 @@
 // import React, { useState } from "react";
 import cart from "../assets/2page/cart.png";
-import Option from './FormdropDownoption/option'
+import Option from "./FormdropDownoption/option";
 import Input from "./FormdropDownoption/TextFields";
+import { Button } from "@chakra-ui/react";
+import Right from "../assets/2page/rigth mark.png";
+import "./FormdropDownoption/Form.css";
 // import { useState } from "react";
 
 function StockForm() {
@@ -24,11 +27,51 @@ function StockForm() {
           <h3>ADD ITEMS</h3>
         </div>
         <div>
-          <Option Heading={"SITE NAME"} Placeholder={"Select the site Name"} Value1={"Mankulam"} Value2={"Colombo"} Value3={"Madirigiriya"}/>
-          <Option Heading={"ITEM"} Placeholder={"Enter your Item type"} Value1={"Metal"} Value2={"Stone"} Value3={"Cement"}/>
-          <Option Heading={"SUB-CATERGORY"} Placeholder={"Select Your Sub Category"} Value1={"Metal"} Value2={"Stone"} Value3={"Cement"}/>
-          <Input Placeholder={"Enter the quantity"} Heading={"Quantity"} />
+          <Option
+            Heading={"SITE NAME"}
+            Placeholder={"Select the site Name"}
+            Value1={"Mankulam"}
+            Value2={"Colombo"}
+            Value3={"Madirigiriya"}
+          />
+          <Option
+            Heading={"ITEM"}
+            Placeholder={"Enter your Item type"}
+            Value1={"Metal"}
+            Value2={"Stone"}
+            Value3={"Cement"}
+          />
+          <Option
+            Heading={"SUB-CATERGORY"}
+            Placeholder={"Select Your Sub Category"}
+            Value1={"Metal"}
+            Value2={"Stone"}
+            Value3={"Cement"}
+          />
+          <Input Placeholder={"Enter the quantity"} Heading={"QUANTITY"} />
           <Input Placeholder={"Price of a Quantity "} Heading={"ONE OF PER"} />
+        </div>
+        <div className="btn-sum-lay">
+          <h3 className="Sum-txt">SUM : </h3>
+          <Button
+            w="6.5rem"
+            marginBottom=".8rem"
+            marginLeft="7.5rem"
+            fontSize="0.8rem"
+            colorScheme="teal"
+            variant="solid"
+          >
+            {" "}
+            <img
+              style={{
+                width: "1.7rem",
+                marginRight: ".6rem",
+              }}
+              src={Right}
+              alt=""
+            />{" "}
+            <div style={{ marginRight: "0.8rem" }}>CONFIRM</div>
+          </Button>
         </div>
       </div>
     </div>
