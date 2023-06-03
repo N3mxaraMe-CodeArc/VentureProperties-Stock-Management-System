c636f960ba95cf48efa1e04edc74525916dc954a
N3mxara <nimsara.20223294@iit.ac.lk>
2023-06-03 09:19:34 +0530

    git push

--- src/Component/SecStockForm.jsx
+++ src/Component/SecStockForm.jsx
@@ -1,1 +1,18 @@
-ffc
+
+
+
+function StockForm() {
+    return ( 
+        <div>
+            <div className="container">
+                <div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default StockForm;
