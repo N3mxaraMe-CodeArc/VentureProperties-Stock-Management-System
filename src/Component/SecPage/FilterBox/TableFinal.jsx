9497364253179df727e73d6958f907f9cb60f45e
N3mxara <nimsara.20223294@iit.ac.lk>
2023-06-12 09:18:53 +0530

    ij

--- src/Component/SecPage/FilterBox/TableFinal.jsx
+++ src/Component/SecPage/FilterBox/TableFinal.jsx
@@ -4,7 +4,7 @@ import TableHead from './TableHeading';
 function TableFinal() {
     return ( 
         <div>
-            <TableHead/>
+            <TableHead/> 
             <Table SiteName="Colombo" Catergory="Cement" SubCatergory="Sanstha" DateApp="23/10/2023" Quantity="200" Price="1000" Name="Nimsara" Total="Rs.380,000"/>
             <Table SiteName="Colombo" Catergory="Cement" SubCatergory="Sanstha" DateApp="23/10/2023" Quantity="200" Price="1000" Name="Nimsara" Total="Rs.380,000"/>
             <Table SiteName="Colombo" Catergory="Cement" SubCatergory="Sanstha" DateApp="23/10/2023" Quantity="200" Price="1000" Name="Nimsara" Total="Rs.380,000"/>
